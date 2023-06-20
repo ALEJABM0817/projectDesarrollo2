c6c2bd034c3d503e467191654ee31749fa228313
camilaarcos <115505484+camilaarcos@users.noreply.github.com>
2023-06-20 14:19:00 -0500

    servicios incluidos

---
+++ create_services.py
@@ -0,0 +1,20 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangocrud.settings")
+
+import django
+django.setup()
+
+from tasks.models import Servicio
+
+def create_service(nombre, precio):
+    servicio = Servicio(nombre=nombre, precio=precio)
+    servicio.save()
+    print(f"Servicio {nombre} creado correctamente.")
+
+if __name__ == "__main__":
+    create_service("DJ", 100.0)
+    create_service("Hora Loca", 50.0)
+    create_service("Utilería ", 50.0)
+    create_service("Decoración", 200.0)
+    create_service("Meseros", 150.0)
+    create_service("Bar", 80.0)
