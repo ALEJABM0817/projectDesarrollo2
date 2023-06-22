497f90b02022dcd415b88d8db8edaa0b4c3d5d8f
camilaarcos <115505484+camilaarcos@users.noreply.github.com>
2023-06-21 22:53:54 -0500

    eventos, servicios, alimentos actualizados

---
+++ create_foods.py
@@ -0,0 +1,24 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangocrud.settings")
+
+import django
+django.setup()
+
+from tasks.models import Alimentos
+
+def create_foods(nombre, precio):
+    alimentos =Alimentos(nombre=nombre, precio=precio)
+    alimentos.save()
+    print(f"Alimento o Bebida {nombre} creado correctamente.")
+
+if __name__ == "__main__":
+    create_foods("Plato1", 100.0)
+    create_foods("Plato2", 100.0)
+    create_foods("Comida Rápida1", 50.0)
+    create_foods("Comida Rápida2", 50.0)
+    create_foods("Jugo1", 20.0)
+    create_foods("Jugo2", 20.0)
+    create_foods("Limonada", 10.0)
+    create_foods("Gaseosa", 50.0)
+    create_foods("Licor", 300.0)
+    create_foods("Snacks", 200.0)
