f4fe4fa1a47e0e19a5b0853e749f2598763fc821
camilaarcos <maria.arcos@correounivalle.edu.co>
2023-06-30 10:52:47 -0500

    Calendarioa actualizado

--- create_reservas.py
+++ create_reservas.py
@@ -16,12 +16,21 @@ def create_reservas(titulo, usuario, servicios, eventos,
     print(f"reservación {titulo} creado correctamente.")
 
 if __name__ == "__main__":
-    create_reservas("Cumpleaños ana","ana", "DJ Hora Loca Meseros", "Cumpleaños",
-                    "Comida Rápida1 Gaseosa Licor", "50", "2023-06-07")
-    create_reservas("Cena Ejecutiva","maria", "Utilería Meseros", "Ejecutivos",
-                    "Plato2 Jugo", "30", "2023-06-30")
-    create_reservas("Quinces Lina","ryan", "DJ Utilería Hora Loca Meseros Decoración Bar", "Quinceaños",
-                    "Plato1 Gaseosa Licor", "130", "2023-06-28")
-    create_reservas("Graduación 2023","Camil", "DJ Hora Loca Meseros Decoración Bar", "Grados",
-                    "Comida Rápida2 Licor Gaseosa", "80", "2023-06-29")
+    create_reservas("Bautizo","ana", "DJ Hora Loca Meseros", "Bautizo",
+                    "Comida Rápida1 Gaseosa Licor", "50", "2023-06-15")
+    create_reservas("ejecutivo","maria", "Utilería Meseros", "Ejecutivos",
+                    "Plato2 Jugo", "30", "2023-06-01")
+    create_reservas("Cumpleaños","ryan", "DJ Utilería Hora Loca Meseros Decoración Bar", "Cumpleaños",
+                    "Plato1 Gaseosa Licor", "130", "2023-06-25")
+    create_reservas("Quinceaños","Camil", "DJ Hora Loca Meseros Decoración Bar", "Quinceaños",
+                    "Comida Rápida2 Licor Gaseosa", "80", "2023-06-03")
+    create_reservas("Cumpleañoss","petro", "DJ Utilería Hora Loca Meseros Decoración Bar", "Cumpleaños",
+                    "Plato2 Gaseosa Licor", "130", "2023-06-17")
+    create_reservas("Bodas","Camil", "DJ Hora Loca Meseros Decoración Bar", "Grados",
+                    "Comida Rápida2 Licor Gaseosa", "80", "2023-06-14")
+    create_reservas("Fiesta","ryan", "DJ Utilería Hora Loca Bar", "Cumpleaños",
+                    "Comida Rápida2 Gaseosa Licor", "130", "2023-06-20")
+    create_reservas("Eventos","ana", "DJ Hora Loca Meseros Decoración Bar", "Grados",
+                    "Comida Rápida2 Licor Gaseosa", "80", "2023-05-28")
+
 
